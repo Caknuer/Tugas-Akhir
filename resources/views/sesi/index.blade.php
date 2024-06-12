5cf0334f72f68ad9af571272f75c32a0a8de5184
oy <caknuroy@gmail.com>
2024-06-12 13:04:57 +0700

    update terbaru

--- resources/views/sesi/index.blade.php
+++ resources/views/sesi/index.blade.php
@@ -10,19 +10,21 @@
             @method("POST")
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" value="{{Session::get('email')}}" name="email" class="form-control">
+                <input type="email" value="{{Session::get('email')}}" name="email" class="form-control" placeholder="email@gmail.com">
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control" placeholder="*********">
             </div>
             <div class="mb-3 d-grid">
                 <button name="submit" type="submit" class="btn btn-primary">Login</button>
             </div>
-            <li class="nav-item">
-                <a>Silahkan daftar terlebih dahulu ;)</a>
-                <a href="/sesi/register" class="nav-link center border mx-auto" type="submit">Sing in-</a>
-            </li>
+            <div>
+                <li class="nav-item">
+                    <a>Silahkan daftar terlebih dahulu ;)</a>
+                    <button href="/sesi/register" class="btn btn-primsry text-center border mx-auto" type="submit">Sing in</button>
+                </li>
+            </div>
         </form>
     </div>
 @endsection
